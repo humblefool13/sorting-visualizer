26b678473b0ff1049c3267848778d0da5d4eb957
humblefool13 <akshatksyp13@gmail.com>
2023-11-07 00:16:40 +0530

    responsivity

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import './App.css'
-import Visualizer from './components/Visualizer'
+import React from "react";
+import "./App.css";
+import "./Media.css";
+import Visualizer from "./components/Visualizer";
 
 function App() {
   return (
     <>
       <Visualizer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
